dcd13f35f97b227bec86dccdfe1b580d35ac7de6
Priyam <priyam009@live.com>
2019-11-05 08:07:00 +1100

    Update Background component and Add Logo, Modal, GoogleSignin Component

--- client/src/components/Basics/index.js
+++ client/src/components/Basics/index.js
@@ -1,6 +1,18 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
-export function Background({ children }) {
-  return <div className="background-content">{children}</div>;
+export function Background({ children, page }) {
+  return <div className={`background-content ${page}-modal`}>{children}</div>;
+}
+
+export function Modal({ children }) {
+  return <div className="modal-content">{children}</div>;
+}
+
+export function Logo() {
+  return <div className="logo">Cool Beans</div>;
+}
+
+export function SigninGoogle() {
+  return <div class="g-signin2" data-onsuccess="onSignIn"></div>;
 }
