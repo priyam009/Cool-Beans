f0192f2a26c6a674820022bd58115f925877c70c
Priyam <priyam009@live.com>
2019-11-04 22:14:14 +1100

    Add Signin page Background component

---
+++ client/src/pages/Signin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import {Background} from "../components/Basics";
+
+class Signin extends Component {
+  render() {
+    return (
+      <Background>
+        <div className="text-light">Hello World</div>
+      </Background>
+    );
+  }
+}
+
+export default Signin;
