36465373d037a1dcea590e8c34bea93b892fa247
Priyam <priyam009@live.com>
2019-11-06 01:16:33 +1100

    Add comments and refine code

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+//Require Express
 const express = require("express");
+//Require Mongoose
 const mongoose = require("mongoose");
+
 const path = require("path");
 const routes = require("./routes");
+
+//Add Port
 const PORT = process.env.PORT || 3001;
+
+//Initialise Express
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-// Define API routes here
 app.use(routes);
 
 //Connect to Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coolsbeans");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coolbeans");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
