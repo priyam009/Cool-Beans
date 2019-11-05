30f5695e06f6170835309100720cc767e9a337df
Priyam <priyam009@live.com>
2019-11-06 01:15:22 +1100

    Update Component to update state and getURL to redirect on clicking Google Sign in

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import { Background, Modal, Logo, SigninGoogle } from "../components/Basics";
+import API from "../utils/API";
+// import { Link } from "react-router-dom";
 
 class Signin extends Component {
+  state = {
+    isSignedIn: false,
+    url: ""
+  }
+
+  componentDidMount() {
+    this.getLink();
+  }
+
+  getLink = () => {
+    API.getURL()
+      .then(res =>
+        this.setState({url: res.data.url})
+      )
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <Background page="signin">
         <Modal>
           <Logo />
-          <SigninGoogle />
+          <SigninGoogle url={this.state.url}/>
         </Modal>
       </Background>
     );
