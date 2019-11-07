30d6ded00898d16f822f544619263a84327cb1c3
Priyam <priyam009@live.com>
2019-11-07 22:59:03 +1100

    Add and create express session and use create-mongo

--- server.js
+++ server.js
@@ -2,24 +2,38 @@
 const express = require("express");
 //Require Mongoose
 const mongoose = require("mongoose");
+//Initialise Express
+const app = express();
 
-const path = require("path");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const routes = require("./routes");
 
 //Add Port
 const PORT = process.env.PORT || 3001;
 
-//Initialise Express
-const app = express();
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost/coolbeans"
 
 //Connect to Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coolbeans");
+mongoose.connect(mongoUrl);
+
+app.use(session({
+  store: new MongoStore({
+      url: mongoUrl
+  }),
+  secret: "sssshhhh",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+      maxAge: 10 * 60 * 60 * 24
+  }
+}));
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
