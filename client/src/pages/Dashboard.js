3b711f0ffc8fe219fca59faf5dad03d5212c977c
Priyam <priyam009@live.com>
2019-11-07 22:53:10 +1100

    Update component to use cookies

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,27 +1,39 @@
 import React, { Component } from "react";
 import queryString from "query-string";
 import API from "../utils/API";
+import Cookies from 'js-cookie';
 
 class Dashboard extends Component {
   state = {
-    code: ""
+    token: {},
+    userdata: Cookies.get('userData')
   };
 
   componentDidMount() {
     const parsed = queryString.parse(this.props.location.search);
-
+    // console.log(parsed);
     this.getToken(parsed.code);
   }
 
-  getToken = (code) => {
+  getToken = code => {
     API.getToken(code)
-      .then(res => console.log(res))
+      .then(res => 
+        // console.log("token",res.data.token)
+        this.setState({ token: res.data.user })
+        )
       .catch(err => console.log(err));
   };
 
   render() {
-    console.log(this.state.code);
-    return <div>Welcome to the React dashboard</div>;
+    console.log(this.state.userdata);
+    return (
+      <div>
+        <div>Welcome to Dashboard</div>
+        <div>Name: {this.state.userdata}</div>
+        <div>Email: {this.state.userdata.email}</div>
+        <div>Picture: <img src={this.state.token.picture}/></div>
+      </div>
+    );
   }
 }
 
