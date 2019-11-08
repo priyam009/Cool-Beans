9b6711940ffedbed1d1dedea7593cd8fb1f04b34
Priyam <priyam009@live.com>
2019-11-08 23:56:28 +1100

    Add route path for user

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const googleRoutes = require("./signin");
+const userRoutes = require("./user");
 
 router.use("/google", googleRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
