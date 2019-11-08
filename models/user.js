6b906ec206b966d323c8e25362c0eb7f451d42b0
Priyam <priyam009@live.com>
2019-11-09 00:03:44 +1100

    Create user collection

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  picture: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
