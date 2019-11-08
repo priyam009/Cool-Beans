49b8e1c7731f20515386b5f2c417958aae735a57
Priyam <priyam009@live.com>
2019-11-09 00:02:59 +1100

    Update server to add cookie parser package

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 const express = require("express");
 //Require Mongoose
 const mongoose = require("mongoose");
+//Require Cookie Parser
+const cookieParser = require('cookie-parser');
+
 //Initialise Express
 const app = express();
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+// const session = require('express-session');
+// const MongoStore = require('connect-mongo')(session);
+
 const routes = require("./routes");
 
 //Add Port
@@ -15,25 +19,28 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(routes);
 
 const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost/coolbeans"
 
 //Connect to Mongo DB
 mongoose.connect(mongoUrl);
 
-app.use(session({
-  store: new MongoStore({
-      url: mongoUrl
-  }),
-  secret: "sssshhhh",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-      maxAge: 10 * 60 * 60 * 24
-  }
-}));
+// app.use(session({
+//   store: new MongoStore({
+//       url: mongoUrl
+//   }),
+//   secret: "sssshhhh",
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//       maxAge: 60
+//       // * 1000 * 60 * 60 * 24
+//   }
+// }));
 
-app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
