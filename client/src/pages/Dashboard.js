db0b1645b6a342e5f0dd7a9387e7778d73bcc6ac
Priyam <priyam009@live.com>
2019-11-08 23:52:26 +1100

    Add query string package to extract code from the url

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,37 +1,66 @@
 import React, { Component } from "react";
-import queryString from "query-string";
+// import queryString from "query-string";
 import API from "../utils/API";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 class Dashboard extends Component {
   state = {
-    token: {},
-    userdata: Cookies.get('userData')
+    user: {},
+    id: Cookies.get("token")
   };
 
+  // componentWillMount() {
+  //   const parsed = queryString.parse(this.props.location.search);
+  //   // console.log(parsed);
+  //   this.getToken(parsed.code);
+  // }
+
+  // getToken = code => {
+  //   API.getToken(code)
+  //     .then(res => {
+  //       console.log(res.data);
+  //       this.setState({ user: res.data });
+  //     })
+  //     .catch(err => console.log(err));
+  // };
+
   componentDidMount() {
-    const parsed = queryString.parse(this.props.location.search);
-    // console.log(parsed);
-    this.getToken(parsed.code);
+    this.getUser(this.state.id);
   }
 
-  getToken = code => {
-    API.getToken(code)
-      .then(res => 
-        // console.log("token",res.data.token)
-        this.setState({ token: res.data.user })
-        )
-      .catch(err => console.log(err));
+
+  getUser = id => {
+    if (id) {
+      API.getUser(id)
+        .then(res => {
+          console.log(res.data);
+          this.setState({ user: res.data });
+        })
+        .catch(err => console.log(err));
+    } else {
+      this.props.history.push("/");
+    }
+  };
+
+  handleClick = () => {
+    Cookies.remove("token");
+    this.setState({id:""})
+    this.props.history.push("/");    
   };
 
   render() {
-    console.log(this.state.userdata);
+    console.log("Cookie id: ", Cookies.get("token"));
+    console.log("Props: ", this.props);
+    console.log("Window: ", window);
     return (
       <div>
         <div>Welcome to Dashboard</div>
-        <div>Name: {this.state.userdata}</div>
-        <div>Email: {this.state.userdata.email}</div>
-        <div>Picture: <img src={this.state.token.picture}/></div>
+        <div>Name: {this.state.user.firstName}</div>
+        <div>Email: {this.state.user.email}</div>
+        <div>
+          Picture: <img src={this.state.user.picture} />
+        </div>
+        <button onClick={this.handleClick}>Log Out</button>
       </div>
     );
   }
--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { Background, Modal, Logo, SigninGoogle } from "../components/Basics";
 import API from "../utils/API";
+import queryString from "query-string";
 // import { Link } from "react-router-dom";
 
 class Signin extends Component {
+
   state = {
     isSignedIn: false,
     url: ""
   }
 
+  // componentWillMount() {
+  // }
+  
   componentDidMount() {
     this.getLink();
+    const parsed = queryString.parse(this.props.location.search);
+    // console.log(parsed);
+    this.getToken(parsed.code);
   }
 
   getLink = () => {
