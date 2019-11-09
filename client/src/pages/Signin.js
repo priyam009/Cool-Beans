424e0de33f35dd6bdb8854e8bf20dd6f362a758c
Priyam <priyam009@live.com>
2019-11-09 22:53:05 +1100

    Add comments

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -6,24 +6,31 @@ import { Spinner } from "../components/Addons";
 
 class Signin extends Component {
   state = {
-    url: "",
-    loading: false
+    url: "", //To store URL for Google Sign-in redirect
+    loading: false //To check if Signed-in to Google
   };
 
   componentDidMount() {
+    //Get Google Sign-in Link to attach to Sign-in Button
     this.getLink();
+
+    //Parse the code from Google using queryString package from page location
     const parsed = queryString.parse(this.props.location.search);
+
+    //Get Token from the parsed code from Google if parsed code exists 
     if (parsed.code) {
       this.getToken(parsed.code);
     }
   }
 
+  //API call to get URL link from Google to redirect to Google Sign-in Page
   getLink = () => {
     API.getURL()
       .then(res => this.setState({ url: res.data.url }))
       .catch(err => console.log(err));
   };
 
+  //API call to get token from Google with JWT Signature, set loading state to true and redirect to Dashboard
   getToken = code => {
     API.getToken(code)
       .then(res => {
