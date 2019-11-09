d173d1476f11cb9f2b97e59621f4324a11acea90
Priyam <priyam009@live.com>
2019-11-09 22:34:48 +1100

    Add Spinner to show only after Signing in

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -2,56 +2,49 @@ import React, { Component } from "react";
 import { Background, Modal, Logo, SigninGoogle } from "../components/Basics";
 import API from "../utils/API";
 import queryString from "query-string";
-// import { Link } from "react-router-dom";
+import { Spinner } from "../components/Addons";
 
 class Signin extends Component {
-
   state = {
-    isSignedIn: false,
-    url: ""
-  }
+    url: "",
+    loading: false
+  };
 
-  // componentWillMount() {
-  // }
-  
   componentDidMount() {
     this.getLink();
     const parsed = queryString.parse(this.props.location.search);
-    // console.log(parsed);
-    this.getToken(parsed.code);
+    console.log("Parsed: ", parsed);
+    if (parsed.code) {
+      this.getToken(parsed.code);
+    }
   }
 
   getLink = () => {
     API.getURL()
-      .then(res =>
-        this.setState({url: res.data.url})
-      )
+      .then(res => this.setState({ url: res.data.url }))
       .catch(err => console.log(err));
-  }
+  };
 
   getToken = code => {
     API.getToken(code)
       .then(res => {
-        console.log(res.data);
-        // this.setState({ user: res.data });
-        setTimeout(
-          () => this.props.history.push('/dashboard'),
-          2000
-        );
-        
+        this.setState({
+          loading: true
+        });
+        setTimeout(() => this.props.history.push("/dashboard"), 2000);
       })
       .catch(err => console.log(err));
   };
 
-
   render() {
-    // console.log("url", this.state.url)
+    console.log("Loading: ", this.state.loading);
     return (
       <Background page="signin">
         <Modal>
           <Logo />
-          <SigninGoogle url={this.state.url}/>
+          <SigninGoogle url={this.state.url} />
         </Modal>
+        <Spinner loading={this.state.loading} />
       </Background>
     );
   }
