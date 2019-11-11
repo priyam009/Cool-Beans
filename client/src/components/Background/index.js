18684f313b444b6723a5ba370d34e0b282fc347a
Priyam <priyam009@live.com>
2019-11-11 20:24:15 +1100

    Renamed file to Basics

--- client/src/components/Background/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export function Background({ children, page }) {
-  return <div className={`background-content ${page}-modal`}>{children}</div>;
-}
-
-
