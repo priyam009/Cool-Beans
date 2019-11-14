826b841532ec57f4beafa0667014cd4e092b8d1a
Priyam <priyam009@live.com>
2019-11-14 23:21:39 +1100

    Add component to create Form

---
+++ client/src/components/Form/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+
+export function Form({ children }) {
+  return <form>{children}</form>;
+}
+export function NGOForm(props) {
+  console.log("NGOform", props);
+  return (
+    <>
+      <div>
+        <label>Enter Name:</label>
+        <input
+          value={props.name}
+          name="ngoName"
+          onChange={event => props.handleInputChange(event)}
+          type="text"
+          placeholder="(Required)"
+        />
+      </div>
+      <div>
+        <label>Enter Purpose:</label>
+        <textarea
+          value={props.purpose}
+          name="ngoPurpose"
+          onChange={event => props.handleInputChange(event)}
+          type="text"
+          placeholder="(Required)"
+        />
+      </div>
+      <button
+        className="card-link btn btn-dark"
+        onClick={event => props.handleNGOSubmit(event, props)}
+      >
+        Add NGO
+      </button>
+    </>
+  );
+}
+
+export function EmployeeForm(props) {
+  console.log("Employee", props);
+  return (
+    <>
+      <div>
+        <label>Enter First Name:</label>
+        <input
+          value={props.firstName}
+          onChange={props.handleInputChange}
+          name="firstName"
+          placeholder="(Required)"
+        />
+      </div>
+      <div>
+        <label>Enter Job Title:</label>
+        <input
+          value={props.title}
+          onChange={props.handleInputChange}
+          name="title"
+          placeholder="(Required)"
+        />
+      </div>
+      <div className="d-flex">
+        <label>NGO to support: </label>
+        <div>
+          {props.supports.map((support, index) => (
+            <Checkbox 
+            key={index}
+            name={support.name}
+            isChecked={support.isChecked}
+            handleCheckboxChange={props.handleCheckboxChange}
+            />
+          ))}
+        </div>
+      </div>
+      <button
+        className="card-link btn btn-dark"
+        onClick={event => props.handleEmployeeSubmit(event, props)}
+      >
+        Add Employee
+      </button>
+    </>
+  );
+}
+
+export function Checkbox(props) {
+  console.log("checkbox props", props);
+  return (
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value={props.name}
+        onChange={props.handleCheckboxChange}
+        checked={props.isChecked}
+        id="defaultCheck1"
+      />
+      <label className="form-check-label" htmlFor="defaultCheck1">
+        {props.support}
+      </label>
+    </div>
+  );
+}
