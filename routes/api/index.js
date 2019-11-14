4a4b7ad5c241a11a228a555abb8c9fbd61f6dbdf
Priyam <priyam009@live.com>
2019-11-15 00:04:47 +1100

    Route for NGO

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const googleRoutes = require("./signin");
 const userRoutes = require("./user");
+const ngoRoutes = require("./ngo");
 
 router.use("/google", googleRoutes);
 router.use("/user", userRoutes);
+router.use("/ngo", ngoRoutes);
 
 module.exports = router;
