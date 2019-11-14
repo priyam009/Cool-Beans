4cfe43c910b47fca07be6d026e9f4f69e933b146
Priyam <priyam009@live.com>
2019-11-14 23:21:00 +1100

    Add NGO and Employee buttons and forms for each

--- client/src/pages/Add.js
+++ client/src/pages/Add.js
@@ -42,18 +42,97 @@ class Add extends Component {
     }
   };
 
+  handleActiveState = active => {
+    this.setState({
+      active: active !== "ngo" ? "employee" : "ngo"
+    });
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleNGOSubmit = (event, ngo) => {
+    event.preventDefault();
+    console.log("ngo", ngo);
+  };
+
+  handleEmployeeSubmit = (event, employee) => {
+    event.preventDefault();
+    console.log("employee", employee);
+  };
+
+  handleCheckboxChange = event => {
+    event.preventDefault();
+    const target = event.target;
+
+    console.log("event.target", target.value);
+    console.log("event.checked", target.checked);
+  };
+
   render() {
-    console.log("Cookie id: ", Cookies.get("token"));
-    console.log("Props: ", this.props);
-    console.log("Window: ", window);
+    // console.log("Cookie id: ", Cookies.get("token"));
+    // console.log("Props: ", this.props);
+    // console.log("Window: ", window);
+    // console.log("Active State: ", this.state.active);
 
     return (
       <Background page="dashboard">
-        <Navigation props={this.props}/>
-        <Title title="Add New Employee Or NGO"/>
+        <Navigation props={this.props} />
+        <div className="add-content">
+          <Title title="Add New Employee Or NGO" />
+          <div className="add-content-wrapper">
+            <div className="add-content-button">
+              <div
+                className={
+                  this.state.active === "employee"
+                    ? "active-gradient"
+                    : "inactive-gradient"
+                }
+                onClick={() => this.handleActiveState("employee")}
+              >
+                ADD EMPLOYEE
+              </div>
+              <div
+                className={
+                  this.state.active === "ngo"
+                    ? "active-gradient"
+                    : "inactive-gradient"
+                }
+                onClick={() => this.handleActiveState("ngo")}
+              >
+                ADD NGO
+              </div>
+            </div>
+            <Box location="add">
+              <Form>
+                {this.state.active === "ngo" ? (
+                  <NGOForm
+                    name={this.state.ngoName}
+                    purpose={this.state.ngoPurpose}
+                    handleInputChange={this.handleInputChange}
+                    handleNGOSubmit={this.handleNGOSubmit}
+                  />
+                ) : (
+                  <EmployeeForm
+                    name={this.state.employeeName}
+                    title={this.state.employeeTitle}
+                    supports={this.state.dbNGO}
+                    handleInputChange={this.handleInputChange}
+                    handleEmployeeSubmit={this.handleEmployeeSubmit}
+                    handleCheckboxChange={this.handleCheckboxChange}
+                  />
+                )}
+              </Form>
+            </Box>
+          </div>
+        </div>
       </Background>
     );
   }
 }
 
 export default Add;
+
+// d-flex flex-column justify-content-around align-items-center
