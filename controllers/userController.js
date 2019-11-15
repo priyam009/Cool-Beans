1c61cab9b8a7088df36ee0ba2c798ef297065592
Priyam <priyam009@live.com>
2019-11-16 10:01:29 +1100

    To getAllNGO from the NGO Database and to createEmployee to add to the Employee database

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,21 +3,33 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   findById: function(req, res) {
-    payload = jwt.verify(req.params.id, process.env.CLIENT_SECRET)
+    payload = jwt.verify(req.params.id, process.env.CLIENT_SECRET);
 
-    db.User
-      .findById(payload.id)
+    db.User.findById(payload.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
 
   createNGO: function(req, res) {
-    console.log("createNGO", req.body.ngo);
-    const dbNGO = req.body.ngo
-    db.Ngo
-    .create(dbNGO)
+    const dbNGO = req.body.ngo;
+    db.Ngo.create(dbNGO)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  getAllNGO: function(req, res) {
+    db.Ngo.find({})
+      .then(dbModel => {
+        res.json(dbModel);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+
+  createEmployee: function(req, res) {
+    const dbEmployee = req.body.employee;
+    // console.log("dbEmployee", dbEmployee)
+    db.Employee.create(dbEmployee)
     .then(dbModel => res.json(dbModel))
     .catch(err => res.status(422).json(err));
-    
   }
-}
+};
