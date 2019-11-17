f0aff7df2644b15512b7cfc0024e44be018d8688
Priyam <priyam009@live.com>
2019-11-17 22:40:36 +1100

    Handle - Open and Close modal, Clear order on modal close, Update state modal -ngo and employee values on modal open, Handle add order and remove order, Update state- total and ngoContri on order change, Update the actual order on change, Submit Order and add or upate employee, ngo values to database on order submit

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -70,12 +70,143 @@ class Dashboard extends Component {
     }
   };
 
-  render() {
-    console.log("Cookie id: ", Cookies.get("token"));
-    console.log("Props: ", this.props);
-    console.log("Window: ", window);
-    console.log("User: ", this.state.user);
+  //Handle Modal Close
+  handleClose = () => {
+    this.setState({
+      show: false,
+      total: 0,
+      ngoContri: 0
+    });
+    this.clearOrder();
+  };
 
+  //Clear current order on Modal Close
+  clearOrder = () => {
+    this.setState(state => {
+      const coffee = state.coffee.map(item => {
+        if (item.qty) {
+          return (item.qty = 0);
+        } else {
+          return item.qty;
+        }
+      });
+      return coffee;
+    });
+  };
+
+  //Handle Modal Show
+  handleShow = event => {
+    const id = event.target.id;
+    const employee = this.state.employee.filter(item => item._id === id)[0];
+
+    const ngo = employee.ngo.map(
+      emp => this.state.ngo.filter(org => org._id === emp)[0]
+    );
+
+    this.setState({
+      show: true,
+      modal: {
+        employee: employee,
+        ngo: ngo
+      }
+    });
+  };
+
+  //Add item to the Order
+  handleOrderAdd = event => {
+    event.preventDefault();
+    const target = event.target;
+
+    this.setState(state => {
+      const coffee = state.coffee.map(item => {
+        if (item.name === target.name) {
+          this.updateTotal(item.price, target.value);
+          return item.qty++;
+        } else {
+          return item.isChecked;
+        }
+      });
+      return coffee;
+    });
+  };
+
+  //Update Total and Ngo Contribution
+  updateTotal = (price, action) => {
+    var total = 0;
+    var ngoContri = 0;
+    if (action === "add") {
+      total = this.state.total + price;
+      ngoContri = this.state.ngoContri + price * 0.25;
+    } else {
+      total = this.state.total - price;
+      ngoContri = this.state.ngoContri - price * 0.25;
+    }
+
+    this.setState({
+      total: total,
+      ngoContri: ngoContri
+    });
+  };
+
+  //Remove item from Order
+  handleOrderRemove = event => {
+    event.preventDefault();
+    const target = event.target;
+
+    this.setState(state => {
+      const coffee = state.coffee.map(item => {
+        if (item.name === target.name) {
+          this.updateTotal(item.price);
+          return item.qty--;
+        } else {
+          return item.isChecked;
+        }
+      });
+      return coffee;
+    });
+  };
+
+  //On Order
+  handleSubmitOrder = (currentEmp, currentNgo) => {
+    if (this.state.total) {
+      const dbEmployee = {
+        total: this.state.total + currentEmp.total,
+        contribution: this.state.ngoContri + currentEmp.contribution
+      };
+      const dbNGO = [];
+
+      currentNgo.map((item, i) => {
+        var newNGO = {
+          id: item._id,
+          total: item.total + this.state.ngoContri / currentNgo.length
+        };
+        dbNGO.push(newNGO);
+      });
+
+      this.updateEmployee(dbEmployee, currentEmp._id);
+
+      this.updateNgo(dbNGO);
+
+      this.handleClose();
+    }
+  };
+
+  updateEmployee = (dbEmployee, id) => {
+    API.updateEmployee(dbEmployee, id)
+      .then(res => this.getUser(this.state.id))
+      .catch(err => console.log(err));
+  };
+
+  updateNgo = dbNGO => {
+    dbNGO.map((item, i) => {
+      console.log("item.total", item.total)
+      API.updateNGO(item.total, item.id)
+        .then(res => this.getUser(this.state.id))
+        .catch(err => console.log(err));
+    });
+  };
+
+  render() {
     return (
       <Background page="dashboard">
         <Navigation props={this.props} />
