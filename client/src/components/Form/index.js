fb6cb6f100fdf776978aa3aee7126ea9f2c0d35d
Priyam <priyam009@live.com>
2019-11-19 18:18:48 +1100

    Refactor Ngo and Employee Form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,95 +1,103 @@
 import React from "react";
+import "./style.css";
 
 export function Form({ children }) {
-  return <form>{children}</form>;
+  return <form className="add-form">{children}</form>;
 }
 export function NGOForm(props) {
-  console.log("NGOform", props);
   return (
     <>
-      <div>
-        <label>Enter Name:</label>
-        <input
-          value={props.name}
-          name="ngoName"
-          onChange={event => props.handleInputChange(event)}
-          type="text"
-          placeholder="(Required)"
-        />
+      <div className="form-details">
+        <div className="form-details-div">
+          <label>Enter Name:</label>
+          <input
+            value={props.name}
+            name="ngoName"
+            onChange={event => props.handleInputChange(event)}
+            type="text"
+            placeholder="(Required)"
+          />
+        </div>
+        <div className="form-details-div add-margin">
+          <label>Enter Purpose:</label>
+          <textarea
+            value={props.purpose}
+            name="ngoPurpose"
+            onChange={event => props.handleInputChange(event)}
+            type="text"
+            placeholder="(Required)"
+          />
+        </div>
       </div>
-      <div>
-        <label>Enter Purpose:</label>
-        <textarea
-          value={props.purpose}
-          name="ngoPurpose"
-          onChange={event => props.handleInputChange(event)}
-          type="text"
-          placeholder="(Required)"
-        />
+      <div className="form-details">
+        <button
+          className="form-button"
+          onClick={event => props.handleNGOSubmit(event, props)}
+        >
+          Add NGO
+        </button>
+        <div className="update">{props.update}</div>
       </div>
-      <button
-        className="card-link btn btn-dark"
-        onClick={event => props.handleNGOSubmit(event, props)}
-      >
-        Add NGO
-      </button>
-      <div>{props.update}</div>
     </>
   );
 }
 
 export function EmployeeForm(props) {
-  console.log("Employee", props);
   return (
     <>
-      <div>
-        <label>Enter First Name:</label>
-        <input
-          value={props.name}
-          name="employeeName"
-          onChange={event => props.handleInputChange(event)}
-          placeholder="(Required)"
-        />
-      </div>
-      <div>
-        <label>Enter Job Title:</label>
-        <input
-          value={props.title}
-          onChange={event => props.handleInputChange(event)}
-          name="employeeTitle"
-          placeholder="(Required)"
-        />
-      </div>
-      <div className="d-flex">
-        <label>NGO to support: </label>
-        <div>
-          {props.supports.map((support, index) => (
-            <Select
-              key={index}
-              name={support.name}
-              isChecked={support.isChecked}
-              handleNGOSelect={props.handleNGOSelect}
-            />
-          ))}
+      <div className="form-details">
+        <div className="form-details-div">
+          <label>Enter First Name:</label>
+          <input
+            value={props.name}
+            name="employeeName"
+            onChange={event => props.handleInputChange(event)}
+            placeholder="(Required)"
+          />
+        </div>
+        <div className="form-details-div add-margin">
+          <label>Enter Job Title:</label>
+          <input
+            value={props.title}
+            onChange={event => props.handleInputChange(event)}
+            name="employeeTitle"
+            placeholder="(Required)"
+          />
+        </div>
+        <div className="form-details-div add-margin">
+          <label>NGO to support: </label>
+          <div className="ngo-selection">
+            {props.supports.map((support, index) => (
+              <Select
+                key={index}
+                name={support.name}
+                isChecked={support.isChecked}
+                handleNGOSelect={props.handleNGOSelect}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <button
-        className="card-link btn btn-dark mt-2"
-        onClick={event => props.handleEmployeeSubmit(event)}
-      >
-        Add Employee
-      </button>
-      <div>{props.update}</div>
+      <div className="form-details">
+        <button
+          className="form-button"
+          onClick={event => props.handleEmployeeSubmit(event)}
+        >
+          Add Employee
+        </button>
+        <div className="update">{props.update}</div>
+      </div>
     </>
   );
 }
 
 export function Select(props) {
-  // console.log(props)
-  const button = props.isChecked ? "card-link btn btn-dark" : "card-link btn btn-light"
+  const button = props.isChecked
+    ? "select-button"
+    : "unselect-button";
   return (
     <button
-      className= {button}
+      className={button}
       value={props.name}
       onClick={event => props.handleNGOSelect(event, props)}
     >
