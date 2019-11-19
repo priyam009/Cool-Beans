f6360b76c5c09da04cf9226409f244c22e8c7077
Priyam <priyam009@live.com>
2019-11-19 18:44:40 +1100

    Add production environment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 //Require Mongoose
 const mongoose = require("mongoose");
 //Require Cookie Parser
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 //Initialise Express
 const app = express();
@@ -21,26 +21,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use(routes);
 
-const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost/coolbeans"
+const mongoUrl = process.env.MONGODB_URI || "mongodb://localhost/coolbeans";
 
 //Connect to Mongo DB
 mongoose.connect(mongoUrl);
-
-// app.use(session({
-//   store: new MongoStore({
-//       url: mongoUrl
-//   }),
-//   secret: "sssshhhh",
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: {
-//       maxAge: 60
-//       // * 1000 * 60 * 60 * 24
-//   }
-// }));
-
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
